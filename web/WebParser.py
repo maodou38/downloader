6b77107e0926bcdfea6137078f85718778e041cf
roiding <dingran@ran-ding.ga>
2020-08-24 20:58:52 +0800

    因为lxml构建docker后太大了，修改代码直接使用正则表达式

--- web/WebParser.py
+++ web/WebParser.py
@@ -3,8 +3,8 @@ from urllib import parse
 import requests
 import re
 from faker import Faker
-from lxml import html
-
+# 由于lxml需要用到系统库 非常大
+# from lxml import html
 
 urllib3.disable_warnings()
 
@@ -26,12 +26,13 @@ class Parser:
     def parseMayiyingshi(self):
         # 开始读取第一次的源地址
         url = self.getScript(self.url, 'player_data')
-        index = self.getScript(url, "dp")
+        index = self.getScript(url, "index.m3u8")
         return parse.urljoin(url, index)
 
     def getScript(self, url, pattern):
         html1 = self.session.get(url).text
-        scripts = html.etree.HTML(html1).xpath('//script/text()')
+        # scripts = html.etree.HTML(html1).xpath('//script/text()')
+        scripts = re.findall("<script[\s\S]*?>[\s\S]*?<\/script>", html1)
         # 获取特定script标签的内容，不包含标签本身
         script = list(filter(lambda x: (pattern in x), scripts))[0]
         pattern = "\"url\":\"(.*?)\""
