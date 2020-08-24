38ab86ebf8697914da70b448db4227ef4e8f60cc
roiding <dingran@ran-ding.ga>
2020-08-24 17:45:28 +0800

    提交代码

---
+++ WebParser.py
@@ -0,0 +1,42 @@
+import urllib3
+from urllib import parse
+import requests
+import re
+from faker import Faker
+from lxml import html
+
+
+urllib3.disable_warnings()
+
+
+class Parser:
+    def __init__(self, url, web):
+        self.url = url
+        self.web = web
+        self.session = requests.Session()
+        self.session.headers.update({'User-Agent': Faker().user_agent()})
+        self.session.verify = False
+
+        self.proxies = {}
+
+    def parse(self):
+        if self.web == 'mayiyingshi':
+            return self.parseMayiyingshi()
+
+    def parseMayiyingshi(self):
+        # 开始读取第一次的源地址
+        url = self.getScript(self.url, 'player_data')
+        index = self.getScript(url, "dp")
+        return parse.urljoin(url, index)
+
+    def getScript(self, url, pattern):
+        html1 = self.session.get(url).text
+        scripts = html.etree.HTML(html1).xpath('//script/text()')
+        # 获取特定script标签的内容，不包含标签本身
+        script = list(filter(lambda x: (pattern in x), scripts))[0]
+        pattern = "\"url\":\"(.*?)\""
+        match = re.search(pattern, script)
+        if not match:
+            pattern = "url:\s?'(.*?)',"
+            match = re.search(pattern, script)
+        return re.sub("\\\\", "", match.group(1))
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask import render_template
+from WebParser import Parser
+import json
+from m3u8 import Downloader
+from concurrent.futures import ThreadPoolExecutor
+
+app = Flask(__name__)
+
+pool = ThreadPoolExecutor(2)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    data = json.loads(request.data)
+    url = data['url']
+    web = data['web']
+    name = data['name']
+    path = data['path']
+    m3u8url = Parser(url, web).parseMayiyingshi()
+    downloader = Downloader(m3u8url, path, name)
+    ret, pbar = downloader.run()
+    pool.submit(downloader.mergeFile, ret, pbar)
+    return jsonify("success")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
